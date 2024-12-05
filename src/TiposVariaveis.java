a8c0eda834074492abb2adcb4fd368f43e642c9c
inacioads23 <jinacio11@gmail.com>
2024-12-05 17:20:13 -0300

    inserido exemplos de variáveis e constantes

--- src/TiposVariaveis.java
+++ src/TiposVariaveis.java
@@ -1,4 +1,21 @@
 
 public class TiposVariaveis {
 
+	public static void main(String[] args) {
+		//Variáveis
+		double salarioMinimo = 2500;
+		
+		short numeroCurto = 1;
+		int numeroNormal = numeroCurto;
+		short numeroCurto2 = (short) numeroNormal;
+		
+		int numero = 5;
+		numero = 10;
+		
+		System.out.println(numero);
+		
+		//Constantes
+		final double VALOR_DE_PI = 3.14;
+		
+	}
 }

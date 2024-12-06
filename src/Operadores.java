0d48c33f86cef66198d7b8753454ad218db0d22b
inacioads23 <jinacio11@gmail.com>
2024-12-05 21:36:52 -0300

    Operador ternário

--- src/Operadores.java
+++ src/Operadores.java
@@ -3,16 +3,22 @@ import java.util.Date;
 public class Operadores {
 	public static void main(String[] args) {
 		/*
-		 * String nome = "INACIO"; int idade = 22; double peso = 68.5; char sexo = 'M';
-		 * boolean doadorOrgao = false; Date dataNascimento = new Date(); // classe que
-		 * representa um tipo.
+		 * String nome = "INACIO";
+		 * int idade = 22;
+		 * double peso = 68.5;
+		 * char sexo = 'M';
+		 * boolean doadorOrgao = false;
+		 * Date dataNascimento = new Date(); // classe que representa um tipo.
 		 * 
-		 * double soma = 10.5 + 15.7; int subtracao = 113 - 25; int multiplicacao = 20 *
-		 * 7; int modulo = 18 % 3; // resto da divisão por 3 double resultado = (10 * 7)
-		 * + (20 / 4);
+		 * double soma = 10.5 + 15.7;
+		 * int subtracao = 113 - 25;
+		 * int multiplicacao = 20 * 7;
+		 * int modulo = 18 % 3; // resto da divisão por 3
+		 * double resultado = (10 * 7) + (20 / 4);
 		 * 
 		 * // O operador "+" quando em String tema função de concatenação String
-		 * nomeCompleto = "LINGUAGEM" + "JAVA"; System.out.println(nomeCompleto);
+		 * nomeCompleto = "LINGUAGEM" + "JAVA";
+		 * System.out.println(nomeCompleto);
 		 * 
 		 * String concatenacao = "?"; System.out.println(concatenacao);
 		 * 
@@ -25,40 +31,68 @@ public class Operadores {
 		 * concatenacao = "1" + 1 + 1 + 1; System.out.println(concatenacao);
 		 * 
 		 * concatenacao = "1" + (1 + 1 + 1); System.out.println(concatenacao);
-		 
+		 * 
+		 * 
+		 * // OPERADORES UNÁRIOS
+		 * int numero = 5;
+		 * 
+		 * System.out.println(- numero); // o número fica negativo por causa do OPERADOR DE NEGAÇÃO. Mas isso não afetou a variável
+		 * System.out.println(numero); // aqui é possível ver que o valor da variável não foi afetado
+		 * 
+		 * // para haver uma mudança eu preciso atribuir um novo valor a variável
+		 * numero = - numero; //torno o valor negativo System.out.println(numero); // agora o
+		 * valor da variável foi afetado
+		 */
 
-		// Operadores unários
-		int numero = 5;
-
-		System.out.println(- numero); // o número fica negativo por causa do OPERADOR DE NEGAÇÃO. Mas isso não afetou a variável
-		System.out.println(numero); // aqui é possível ver que o valor da variável não foi afetado
-
-		// para haver uma mudança eu preciso atribuir um novo valor a variável
-		numero = - numero; //torno o valor negativo
-		System.out.println(numero); // agora o valor da variável foi afetado
-		*/
-		
 		/*
-		//Operadores de repetição 
-		int numero = 5;
-		
-		//numero = numero + 1;
-		numero++;
-		System.out.println(numero);
-		
-		//numero = numero -1;
-		numero--;
-		System.out.println(numero);
-		*/
+		 * //OPERADORES DE REPETIÇÃO
+		 * int numero = 5;
+		 * 
+		 * //numero = numero + 1;
+		 * numero++;
+		 * System.out.println(numero);
+		 * 
+		 * //numero = numero -1;
+		 * numero--;
+		 * System.out.println(numero);
+		 */
 
-		//Operador de negação com boolean
-		boolean variavel = true;
+		/*
+		 * //OPERADOR DE NEGAÇÃO com boolean
+		 * boolean variavel = true;
+		 * 
+		 * System.out.println(!variavel); //irá negar/ inverter o valor. Porém a o valor da variável não foi alterado
+		 * System.out.println(variavel); //O valor da variável não foi alterado
+		 * 
+		 * variavel = !variavel;
+		 * System.out.println(variavel); //Agora o valor da variável foi alterado
+		 */
+
+		/*
+		// OPERADOR TERNÁRIO
+		int a, b;
+		String resultado;
+		a = 5;
+		b = 6;
+
+		// Usando if/ else
+		if (a == b) {
+			resultado = "verdadeiro";
+		} else {
+			resultado = "falso";
+		}
 		
-		System.out.println(!variavel); //irá negar/ inverter o valor. Porém a o valor da variável não foi alterado
-		System.out.println(variavel); //O valor da variável não foi alterado
+		System.out.println(resultado);
+
+		// Com operador Ternário
+		resultado = (a == b) ? "verdadeiro" : "false";
+		System.out.println(resultado);
 		
-		variavel = !variavel;		
-		System.out.println(variavel); //Agora o valor da variável foi alterado
+		resultado = (a == b) ? "1" : "0";
+		System.out.println(resultado);
+		*/
+		
+
 	}
 
 }

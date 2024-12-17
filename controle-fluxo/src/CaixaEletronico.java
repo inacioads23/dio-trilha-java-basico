d336a8a0ce0b8bdd6a5e40aa5bb31c04f06201b5
inacioads23 <jinacio11@gmail.com>
2024-12-17 16:50:42 -0300

    Update no diretorio control-fluxo

--- controle-fluxo/src/CaixaEletronico.java
+++ controle-fluxo/src/CaixaEletronico.java
@@ -7,13 +7,14 @@ public class CaixaEletronico {
 		double saldo = 25.0;
 		double valorSolicitado = 22.0;
 		
-		// Condição simples
+		// Condicional simples
 		if(valorSolicitado < saldo) {
 			System.out.printf("Saldo anterior: R$ %.2f ", saldo);
 			System.out.printf("%nValor do saque: R$ %.2f ",valorSolicitado);
 			saldo = saldo - valorSolicitado;
 			
 			System.out.printf("%nNovo saldo: R$ %.2f", saldo);
+		// Condicional composta
 		}else {
 			System.out.println("Saldo insuficiente");
 		}
--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -2,15 +2,16 @@
 public class ResultadoEscolar {
 
 	public static void main(String[] args) {
-		int nota = 8;
+		int nota = 10;
 
 		// Condicional Composta
 		if (nota >= 7) {
 			System.out.println("Aprovado");
+			// Condicional encadeada
+		} else if (nota >= 5) {
+			System.out.println("Recuperação");
 		} else {
 			System.out.println("Reprovado");
 		}
-
 	}
-
 }

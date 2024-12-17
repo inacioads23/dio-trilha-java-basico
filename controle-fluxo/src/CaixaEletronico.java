7325fe7963133d967b0c5785d9fc6b71faf7c175
inacioads23 <jinacio11@gmail.com>
2024-12-17 16:42:58 -0300

    Criado o diretorio control-fluxo

---
+++ controle-fluxo/src/CaixaEletronico.java
@@ -0,0 +1,23 @@
+import java.util.Locale;
+
+public class CaixaEletronico {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		double saldo = 25.0;
+		double valorSolicitado = 22.0;
+		
+		// Condição simples
+		if(valorSolicitado < saldo) {
+			System.out.printf("Saldo anterior: R$ %.2f ", saldo);
+			System.out.printf("%nValor do saque: R$ %.2f ",valorSolicitado);
+			saldo = saldo - valorSolicitado;
+			
+			System.out.printf("%nNovo saldo: R$ %.2f", saldo);
+		}else {
+			System.out.println("Saldo insuficiente");
+		}
+
+	}
+
+}

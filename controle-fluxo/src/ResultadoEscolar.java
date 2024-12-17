372504d833da315160bc3d38a3d4f99f8b4f3899
inacioads23 <jinacio11@gmail.com>
2024-12-17 17:10:15 -0300

    Uso da condição ternária

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -2,16 +2,26 @@
 public class ResultadoEscolar {
 
 	public static void main(String[] args) {
-		int nota = 10;
+		int nota = 7;
 
+		/*
 		// Condicional Composta
 		if (nota >= 7) {
 			System.out.println("Aprovado");
-			// Condicional encadeada
+		// Condicional encadeada
 		} else if (nota >= 5) {
 			System.out.println("Recuperação");
 		} else {
 			System.out.println("Reprovado");
-		}
+		}*/
+		
+		//Condição ternária
+		String resultado = nota >= 7? "Aprovado" : "Reporovado";
+		System.out.println(resultado);
+		
+		nota = 4;
+		
+		String resultado1 = nota >= 7? "Aprovado" : nota >= 5? "Recuperação" : "Reprovado";
+		System.out.println(resultado1);
 	}
 }

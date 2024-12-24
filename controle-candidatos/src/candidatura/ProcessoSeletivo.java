02be82a4fd6c6cd4e4987fac2eb18b852a61e358
inacioads23 <jinacio11@gmail.com>
2024-12-24 07:52:15 -0300

    Update da classe ProcessoSeletivo.java

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,28 +1,62 @@
 package candidatura;
 
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
 	public static void main(String[] args) {
-		imprimirSelecionados();
-		
+		String[] candidatos = { "FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO" };
+		for (String candidato : candidatos) {
+			entrarEmContato(candidato); // utilizando o método
+		}
+
+		// imprimirSelecionados();
 		// selecaoCandidatos();
 		// analisarCandidato(1900.0);
-		//analisarCandidato(2200.0);
-		// analisarCandidato(2000.0);		
+		// analisarCandidato(2200.0);
+		// analisarCandidato(2000.0);
+	}
+
+	static void entrarEmContato(String candidato) {
+		int tentativasRealizadas = 1;
+		boolean continuarTentando = true;
+		boolean atendeu = false;
+
+		do {
+			atendeu = atender(); // utilizando o método
+			continuarTentando = !atendeu;
+			
+			if(continuarTentando) {
+				tentativasRealizadas++;
+			}else {
+				System.out.println("Contato realizado com sucesso");
+			}
+
+		} while (continuarTentando && tentativasRealizadas < 3);
+
+		if (atendeu) {
+			System.out.println("Conseguimos contato com " + candidato + " na " + tentativasRealizadas + "ª tentativa");
+		} else {
+			System.out.println("Não conseguimos contato com " + candidato + ", número máximo de tentativas " + tentativasRealizadas);
+		}
+	}
+
+	static boolean atender() {
+		// se o número gerado de 1 a 3, for igual a 1, ATENDEU
+		return new Random().nextInt(3) == 1;
 	}
 
 	static void imprimirSelecionados() {
 		String[] candidatos = { "FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO" };
-		
+
 		System.out.println("Imprimindo a lista de candidatos informando o indice do elemento");
 		for (int indice = 0; indice < candidatos.length; indice++) {
-			System.out.println("O candidato nº " + (indice +1) + " é " + candidatos[indice]);
+			System.out.println("O candidato nº " + (indice + 1) + " é " + candidatos[indice]);
 		}
-		
+
 		System.out.println("\nForma abreviada de interação - for each");
-		//variável temporária: Array
-		for(String candidato: candidatos) {
+		// variável temporária: Array
+		for (String candidato : candidatos) {
 			System.out.println("O candidato selecionado foi " + candidato);
 		}
 	}
@@ -50,8 +84,9 @@ public class ProcessoSeletivo {
 		}
 	}
 
+	// Método para gerar números aleatórios
 	static double valorPretendido() {
-		// valores gerados entre 1800 e 2200
+		// valores double gerados entre 1800 e 2200
 		return ThreadLocalRandom.current().nextDouble(1800, 2200);
 	}
 

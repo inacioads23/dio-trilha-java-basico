8628fe525192f59315db0a8f31cce890375b81d8
inacioads23 <jinacio11@gmail.com>
2024-12-26 12:02:02 -0300

    Criados os diretórios model e service. O arquivo Cliente.java foi movido para o diretório model; o arquivo ExameService.java foi movido para o diretório service

---
+++ poo/exemplo-pacotes/src/com/mastersoft/clinical/service/ExameService.java
@@ -0,0 +1,11 @@
+package com.mastersoft.clinical.service;
+
+import com.mastersoft.clinical.model.Exame;
+
+public class ExameService {
+	//buscará da classe Exame
+	void salvarExame(Exame exame) {
+		
+	}
+
+}

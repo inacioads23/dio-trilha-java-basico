8715d18ef2a22268801f34850b0d4a51fdc02ba5
inacioads23 <jinacio11@gmail.com>
2024-12-26 11:34:58 -0300

    Criado o diretorio example

---
+++ poo/src/example/Student.java
@@ -0,0 +1,23 @@
+package example;
+import java.awt.Color;
+
+public class Student {
+	String name;
+	int age;
+	Color color;
+	String sex;
+	
+	/*
+	void eating(Food food) {
+		
+	}
+	
+	void drinking(Eat eat) {
+		
+	}
+	
+	void running() {
+		
+	}
+*/
+}

ef89f149d4c8623159282193572f8e0f3560dcf1
inacioads23 <jinacio11@gmail.com>
2024-12-26 11:21:32 -0300

    Criado os arquivos School.java e Student.java

---
+++ poo/src/Student.java
@@ -0,0 +1,22 @@
+import java.awt.Color;
+
+public class Student {
+	String name;
+	int age;
+	Color color;
+	String sex;
+	
+	/*
+	void eating(Food food) {
+		
+	}
+	
+	void drinking(Eat eat) {
+		
+	}
+	
+	void running() {
+		
+	}
+*/
+}

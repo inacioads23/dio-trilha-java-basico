4f6462b7ba855746c272325695d0938b205dd993
inacioads23 <jinacio11@gmail.com>
2025-01-16 13:11:04 -0300

    Gerado Getters e Setters das classes: Aluno.java e Escola.java

--- poo/exemplo-pacotes/src/escolaGetSet/Aluno.java
+++ poo/exemplo-pacotes/src/escolaGetSet/Aluno.java
@@ -1,10 +1,38 @@
 /*
- * Getter: retorna o valor do atributo especificado
+ * Getter: RETORNA (return) o valor do atributo especificado
  * Setter: define outro novo valor para o atributo especificado
  * */
 package escolaGetSet;
 
 public class Aluno {
-	String nome;
-	int idade;
+	private String nome;
+	private int idade;
+	private String sexo;
+
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String newNome) {
+		this.nome = newNome;
+	}
+	public int getIdade() {
+		return idade;
+	}	
+	public void setIdade(int newIdade) {
+		this.idade = newIdade;
+	}	
+	public String getSexo() {		
+		return sexo;
+	}	
+	public void setSexo(String sexo) {
+		if(sexo.equals("M")) {
+			sexo = "Masculino";
+		}else if(sexo.equals("F")) {
+			sexo ="Feminino";
+		}else {
+			sexo = "OUTROS";
+		}
+		this.sexo = sexo;
+	}	
+	
 }
--- poo/exemplo-pacotes/src/escolaGetSet/Escola.java
+++ poo/exemplo-pacotes/src/escolaGetSet/Escola.java
@@ -7,9 +7,23 @@ package escolaGetSet;
 public class Escola {
 	public static void main(String[] args) {
 		Aluno felipe = new Aluno(); // Criando um novo objeto aluno, com nome felipe
-		felipe.nome= "Felipe";
-		felipe.idade= 8;
-		
-		System.out.println("O aluno " + felipe.nome + " tem " + felipe.idade + " anos");
+		/*
+		 // USANDO O PADRÃO NORMAL
+		 // Atribui um novo valor
+		 felipe.nome= "Felipe";
+		 felipe.idade= 8;
+		  
+		 // retorna o valor
+		 System.out.println("O aluno " + felipe.nome + " tem " + felipe.idade + " anos");
+		 */
+
+		// USANDO GET E SET
+		// Atribui um novo valor
+		felipe.setNome("Felipe Jose");
+		felipe.setIdade(17);
+		felipe.setSexo("O");
+
+		// retorna o valor
+		System.out.println("O aluno " + felipe.getNome() + " tem " + felipe.getIdade() + " anos" + " e é do sexo " + felipe.getSexo());
 	}
 }
